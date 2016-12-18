6f31783d556fd9ee5d29df32574211cf7b1ae8a0
Paul <comrade_pasha@ukr.net>
2016-12-18 04:55:07 +0200

    v01 ready

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,18 +1,37 @@
-app.controller('FeedCtrl', ['$scope','FeedService',function ($scope, Feed) {
+app.controller('FeedCtrl', ['$scope','FeedService','$ionicPopup',function ($scope, Feed, $ionicPopup) {
   $scope.name = 'Pavel Tyutyunnik';
   $scope.name2 = function () {
     return 'Vasya';
   };
   $scope.feed = {};
   $scope.loadFeed = function() {
-    //console.log($scope.feed.url);
-    Feed.parseFeed($scope.feed.url).then(function (res) {
-      $scope.feeds = res.data.responseData.feed.entries;
-      console.log(res.data.responseData.feed.entries);
-      //console.log(res);
 
+    Feed.parseFeed($scope.feed.url).then(function (res) {
+      if (res.data.responseData != null) {
+        $scope.error = null;
+        $scope.feeds = res.data.responseData.feed.entries;
+      }
+      else {
+        $scope.error = 'Invalid RSS Feed';
+        $scope.feeds = null;
+      }
     });
   }
+
+  $scope.showError = function (content) {
+    $ionicPopup.alert({
+      title: 'Error',
+      content: content
+    }).then(function (res) {
+      console.log(content);
+    });
+  }
+
+  $scope.clearFeeds = function () {
+    $scope.feeds =null;
+    $scope.feed.url = '';
+  }
+
 }]);/**
  * Created by paul on 15.12.16.
  */
